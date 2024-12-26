78cb5a8f323e471f7c5fbd0826ada88220bcd1da
kleberson154 <kleberson55@hotmail.com>
2024-12-26 18:08:52 -0300

    atualizando fonte

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
-import { Source_Code_Pro } from 'next/font/google'
+import { Fira_Code } from 'next/font/google'
 import './globals.css'
 import PageAnimatePresence from '../components/HOC/PageAnimatePrecense'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
-const sourceCodePro = Source_Code_Pro({
+const firaCode = Fira_Code({
   subsets: ['latin']
 })
 
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={`${sourceCodePro.variable} antialiased`}>
+      <body className={`${firaCode.variable} antialiased`}>
         <PageAnimatePresence>{children}</PageAnimatePresence>
         <SpeedInsights />
       </body>
