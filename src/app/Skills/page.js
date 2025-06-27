e9f89e41141bf5c2c1d9105100c1f334b20f2641
kleberson154 <kleberson55@hotmail.com>
2025-06-27 14:39:56 -0300

    Adicionar os icones nas skills concluido

--- src/app/Skills/page.js
+++ src/app/Skills/page.js
@@ -11,9 +11,26 @@ import JavascriptOriginal from 'react-devicons/javascript/original'
 import TypescriptOriginal from 'react-devicons/typescript/original'
 import ReactOriginal from 'react-devicons/react/original'
 import NextjsOriginal from 'react-devicons/nextjs/original'
-import TailwindcssPlain from 'react-devicons/tailwindcss/plain'
-import BootstrapOriginal from 'react-devicons/bootstrap/original'
+import BootstrapPlain from 'react-devicons/bootstrap/plain'
 import SassOriginal from 'react-devicons/sass/original'
+import TailwindcssOriginal from 'react-devicons/tailwindcss/plain-wordmark'
+import JavaOriginal from 'react-devicons/java/original'
+import SpringOriginal from 'react-devicons/spring/original'
+import PostgresqlOriginal from 'react-devicons/postgresql/original'
+import MongodbOriginalWordmark from 'react-devicons/mongodb/original-wordmark'
+import MysqlOriginalWordmark from 'react-devicons/mysql/original-wordmark'
+import SqliteOriginalWordmark from 'react-devicons/sqlite/original-wordmark'
+import ExpressOriginalWordmark from 'react-devicons/express/original-wordmark'
+import NodejsOriginalWordmark from 'react-devicons/nodejs/original-wordmark'
+import VscodeOriginal from 'react-devicons/vscode/original'
+import GithubOriginal from 'react-devicons/github/original'
+import SpringOriginalWordmark from 'react-devicons/spring/original-wordmark'
+import IntellijOriginalWordmark from 'react-devicons/intellij/original'
+import CodepenOriginalWordmark from 'react-devicons/codepen/original-wordmark'
+import HerokuOriginalWordmark from 'react-devicons/heroku/original-wordmark'
+import KotlinOriginal from 'react-devicons/kotlin/plain-wordmark'
+import AndroidstudioOriginal from 'react-devicons/androidstudio/original'
+import Image from 'next/image'
 
 export default function page() {
   return (
@@ -44,7 +61,7 @@ export default function page() {
         transition={{ duration: 1.5, delay: 1 }}
         className="absolute top-36 flex flex-col xl:flex-row justify-center items-center xl:items-stretch pb-8 xl:px-12 gap-4 "
       >
-        <div className="h-full border-2 border-black p-4 flex flex-col justify-center gap-8 px-8 xl:w-2/6 lg:w-4/5 w-2/3">
+        <div className="border-2  border-black p-4 flex flex-col justify-center gap-8 px-8 xl:w-2/6 lg:w-4/5 w-2/3">
           <h1 className="flex justify-center items-center gap-2 text-2xl">
             <Develope width={34} height={34} /> Front-End Developer
           </h1>
@@ -61,14 +78,23 @@ export default function page() {
               <TypescriptOriginal size="2em" />
               <ReactOriginal size="2em" />
               <NextjsOriginal size="2em" />
-              <TailwindcssPlain size="2em" />
-              <BootstrapOriginal size="2em" />
               <SassOriginal size="2em" />
+              <BootstrapPlain size="2em" />
+              <Image
+                src="https://devicon-website.vercel.app/api/tailwindcss/plain.svg"
+                alt="Kotlin"
+                width={32}
+                height={32}
+              />
             </div>
           </div>
           <div className="text-[calc(0.2em + 1vw)]">
             <strong>FERRAMENTAS</strong>
-            <div className="p-0 m-0">VScode, Github, Codepen etc.</div>
+            <div className="p-0 m-0 flex flex-wrap gap-2">
+              <VscodeOriginal size="2em" />
+              <GithubOriginal size="2em" />
+              <CodepenOriginalWordmark size="2em" />
+            </div>
           </div>
         </div>
         <div className="border-2 border-black p-4 flex flex-col justify-center gap-8 px-8 xl:w-2/6 lg:w-4/5 w-2/3">
@@ -76,19 +102,38 @@ export default function page() {
             <Develope width={34} height={34} /> Back-End Developer
           </h1>
           <p className="text-[calc(0.2em + 1vw)] ">
-            Sou desenvolvedor Java com habilidades voltadas para back-end
-            utilizando o Framework Spring e com habilidades em NodeJs.
+            Sou desenvolvedor Java e Kotlin com habilidades voltadas para
+            back-end e mobile, utilizando o Framework Spring, AndroidStudio e
+            com habilidades em NodeJs.
           </p>
           <div className="text-[calc(0.2em + 1vw)]">
             <strong>HABILIDADES</strong>
-            <div className="p-0 m-0">
-              Java POO, Spring,NodeJs, PostgreSQL, MongoDB, MySQL, ExpressJS,
-              RESTful, JWT.
+            <div className="p-0 m-0 flex flex-wrap gap-2">
+              <JavaOriginal size="2em" />
+              <Image
+                src="https://devicon-website.vercel.app/api/kotlin/original.svg"
+                alt="Kotlin"
+                width={32}
+                height={32}
+              />
+              <SpringOriginal size="2em" />
+              <NodejsOriginalWordmark size="2em" />
+              <PostgresqlOriginal size="2em" />
+              <MongodbOriginalWordmark size="2em" />
+              <MysqlOriginalWordmark size="2em" />
+              <SqliteOriginalWordmark size="2em" />
+              <ExpressOriginalWordmark size="2em" />
             </div>
           </div>
           <div className="text-[calc(0.2em + 1vw)]">
             <strong>FERRAMENTAS</strong>
-            <div className="p-0 m-0">IntelliJ, Github, Spring Initializr.</div>
+            <div className="p-0 m-0 flex flex-wrap gap-2">
+              <IntellijOriginalWordmark size="2em" />
+              <AndroidstudioOriginal size="2em" />
+              <GithubOriginal size="2em" />
+              <SpringOriginalWordmark size="2em" />
+              <HerokuOriginalWordmark size="2em" />
+            </div>
           </div>
         </div>
       </motion.div>
